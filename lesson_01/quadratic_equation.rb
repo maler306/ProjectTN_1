420bd193899628c1a919e46c497d7872b9474b6d
Yerkin <maler306@gmail.com>
2017-05-18 14:37:08 +0600

    Lesson 1/quadratic_equation.rb

--- lesson_01/quadratic_equation.rb
+++ lesson_01/quadratic_equation.rb
@@ -4,13 +4,13 @@ b = gets.chomp.to_f
 c = gets.chomp.to_f
 
 d = b**2 - 4*a*c
-sqroot_d= Math.sqrt(d)
 
 if d<0
   puts "Корней нет, D=#{d}!"
 elsif d==0
   puts "D=#{d}, один корень: x=#{-b/(2*a)}!"
 else
+  sqroot_d= Math.sqrt(d)
   puts "D=#{d}, два корня: x1=#{(-b+sqroot_d)/(2*a)}, x1=#{(-b-sqroot_d)/(2*a)}!"
 end
 
