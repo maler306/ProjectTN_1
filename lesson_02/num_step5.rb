48ee5fbcc0fcba76350981275989ab33d9117555
Yerkin <brisktrade@bk.ru>
2017-05-21 10:47:53 +0600

    Update num_step5.rb
    Заполнить массив числами от 10 до 100 с шагом 5 (а не с 5)

--- lesson_02/num_step5.rb
+++ lesson_02/num_step5.rb
@@ -3,7 +3,5 @@
 #arr5 = Array.new((100-5)/5+1) { |i|  i+1 }.map {|a| 5*a}#work
 #arr55 =Array.new(20) { |i|  i*=5 }#начинается с нуля
 
-arr5_100= (5..100).to_a.select { |num|  num% 5 ==0 }
+arr5_100= (10..100).to_a.select { |num|  num% 5 ==0 }
 p arr5_100
-
-
